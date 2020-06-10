1fa3a67eceea174025a962f36e1c290ca8cc96a8
Yulia Melnyk <melnykalex@gmail.com>
2020-06-10 13:41:32 +0200

    initial

---
+++ Transformation.xslt
@@ -0,0 +1,28 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+                <xsl:output method="xml"/>
+                <xsl:template match="/">
+                                <xsl:element name="series">
+                                                <xsl:for-each select="movies/series/serie">
+                                                                <xsl:element name="serie">
+                                                                                <xsl:attribute name="id">
+                                                                                                <xsl:value-of select="id"></xsl:value-of>
+                                                                                </xsl:attribute>
+                                                                                <xsl:element name="name">
+                                                                                                <xsl:value-of select="name"></xsl:value-of>
+                                                                                </xsl:element>
+                                                                                <xsl:element name="title">
+                                                                                                <xsl:value-of select="desc/title"></xsl:value-of>
+                                                                                </xsl:element>
+                                                                                <xsl:element name="image">
+                                                                                                <xsl:value-of select="images/image[@id = '1']/url"></xsl:value-of>
+                                                                                </xsl:element>
+                                                                                <xsl:element name="review">
+                                                                                                <xsl:value-of select="desc/en"></xsl:value-of>
+                                                                                </xsl:element>
+                                                                </xsl:element>
+                                                </xsl:for-each>
+                                </xsl:element>
+                </xsl:template>
+</xsl:stylesheet>
--- js/main.js
+++ js/main.js
@@ -44,6 +44,10 @@ $('#morning').on('click', function (event) {
   window.location.href = 'http://localhost:8080/proyecto/series/Morning_show.html'
 
 });
+$('#java').on('click', function (event) {
+  window.location.href = 'http://localhost:8038/JavaWebApp_war_exploded'
+
+});
 
 // on Sign Up clicked
 $(document).ready(function () {
@@ -52,203 +56,18 @@ $(document).ready(function () {
   });
 });
 
+//alert window when clicked Subscribe
+$(document).ready(function () {
+  $('#subscribe-btn').on('click', function () {
+    alert('You are subscribed successfully!');
+});
+
+
+
 // on register clicked
 //$(document).ready(function() {	
 //$('#subscribe-btn').on('click', function() {
 //$('.subscribe-frame').css('display', 'block');
 //});
 //});
-
-// put values from xml to Devs.html   
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    devsTitle(this);
-    devsDesc(this);
-    devsIMG(this);
-    littleTitle(this);
-    littleDesc(this);
-    littleIMG(this);
-    darkTitle(this);
-    darkDesc(this);
-    darkIMG(this);
-    manifestTitle(this);
-    manifestDesc(this);
-    manifestIMG(this); 
-    murderTitle(this.responseXML);
-  }
-};
-xhttp.open("GET", "series.xml", true);
-xhttp.send();
-
- // function for get Devs title
-function devsTitle(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("title");
-
-  txt += x[0].childNodes[0].nodeValue + "<br>";
-
-  document.getElementById("devsTitle").innerHTML = txt;
-}
-
-// function for get Devs description
-function devsDesc(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("review");
-
-  txt += x[0].childNodes[0].nodeValue + "<br>";
-
-  document.getElementById("devsDesc").innerHTML = txt;
-}
-// function for get Devs image link
-function devsIMG(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("image");
-
-  txt += x[0].childNodes[0].nodeValue + "<br>";
-
-  document.getElementById("devsImg").getAttribute("src").innerHTML = txt;
-}
-// function for get Little_fires title
-function littleTitle(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("title");
-  for (i = 0; i < x.length; i++) {
-    if (i == 1) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-  }
-  document.getElementById("littleTitle").innerHTML = txt;
-}
-// function for get Little_fires description
-function littleDesc(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("review");
-  for (i = 0; i < x.length; i++) {
-    if (i == 1) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-  }
-
-  document.getElementById("littleDesc").innerHTML = txt;
-}
-// function for get Little_fires link
-function littleIMG(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("image");
-  for (i = 0; i < x.length; i++) {
-    if (i == 1) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-  }
-
-  document.getElementById("littleImg").getAttribute("src").innerHTML = txt;
-}
-// function for get Dark title
-function darkTitle(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("title");
-  for (i = 0; i < x.length; i++) {
-    if (i == 2) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-  }
-  document.getElementById("darkTitle").innerHTML = txt;
-}
-// function for get Dark description
-function darkDesc(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("review");
-  for (i = 0; i < x.length; i++) {
-    if (i == 2) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-  }
-
-  document.getElementById("darkDesc").innerHTML = txt;
-}
-// function for get Dark link
-function darkIMG(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("image");
-  for (i = 0; i < x.length; i++) {
-    if (i == 2) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-  }
-
-  document.getElementById("darkImg").getAttribute("src").innerHTML = txt;
-}
-// function for get Manifest title
-function manifestTitle(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("title");
-  for (i = 0; i < x.length; i++) {
-    if (i == 3) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-  }
-  document.getElementById("manifestTitle").innerHTML = txt;
-}
-// function for get Manifest description
-function manifestDesc(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("review");
-  for (i = 0; i < x.length; i++) {
-    if (i == 3) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-  }
-
-  document.getElementById("manifestDesc").innerHTML = txt;
-}
-// function for get Manifest link
-function manifestIMG(xml) {
-  var x, i, txt, xmlDoc;
-  xmlDoc = xml.responseXML;
-  txt = "";
-  x = xmlDoc.getElementsByTagName("image");
-  for (i = 0; i < x.length; i++) {
-    if (i == 3) {
-      txt += x[i].childNodes[0].nodeValue + "<br>";
-    }
-  }
-
-  document.getElementById("manifestImg").getAttribute("src").innerHTML = txt;
-} 
-// function for get HTGAWM title
-function murderTitle(xmlDoc) {
-  var x, i, txt;
-  txt = "";
-  x = xmlDoc.getElementsByTagName('serie');
-  for (i = 0; i < x.length; i++) {
-      if(x[i].getAttribute('id') == '2'){
-          txt += x[i].children[2].textContent + "<br>";
-      }    
-  }
-  document.getElementById("murderTitle").innerHTML = txt;
-}
-
-
+});
